48deedfd1a914d4482ad3670d982e13c06bf9d35
CatCoders1501 <119710236+CatCoders1501@users.noreply.github.com>
2024-04-30 18:10:28 -0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,12 +1,12 @@
 <?php
-$con=mysqli_connect("example.com","peter","abc123","my_db");
+$con=mysqli_connect("mysqlcluster5.registeredsite.com","rovadmin19","aqrw23Ds1","catcoderdb");
 // Check connection
 if (mysqli_connect_errno())
 {
 echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-$result = mysqli_query($con,"SELECT * FROM Persons");
+$result = mysqli_query($con,"SELECT * FROM meetingdatanew");
 
 echo "<table border='1'>";
 
